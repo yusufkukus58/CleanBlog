3bd657cd821d95f04145b82d0afb201cc7555181
yusuf kukus <ykukus10@gmail.com>
2021-10-18 20:37:55 +0300

    Blogşar eklendi veri tabanına aktarıldı

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const ejs =require('ejs');
-
+const mongoose=require('mongoose');
+const Post = require('./models/Post');
+const path = require('path');
 const app = express();
 
+mongoose.connect('mongodb://localhost/cleanblog-test-db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+ });
+ 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.render('index');
+app.get("/", async (req, res) => {
+  const posts=await Post.find({})
+  res.render('index',{posts});
 });
 app.get("/about", (req, res) => {
   res.render('about');
@@ -17,7 +27,11 @@ app.get("/add", (req, res) => {
   res.render('add');
 });
 app.get("/add_post", (req, res) => {
-    res.render('add_post');
+  res.render('add_post');
+});
+app.post("/posts", async (req, res) => {
+ await Post.create(req.body);
+    res.redirect('/')
   });
 const port =5000;
 app.listen(port , ()=>{
